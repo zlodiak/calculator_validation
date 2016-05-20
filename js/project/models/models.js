3e5a381d2448b0f018c79046d658d0dac10dfbf4
zlodiak <prozaik81-2@yandex.ru>
2016-05-20 17:27:09 +0300

    formatted model pl

--- js/project/models/models.js
+++ js/project/models/models.js
@@ -11,81 +11,45 @@ APP.CalcModel = Backbone.Model.extend({
   }, 
 
   validation: {
-    departCity: [{
-      required: true,
-      msg: 'Поле не может быть пустым'
-    },{
-      minLength: 3,
-      msg: 'Введите не менее 3 сииволов'
-    }], 
+    departCity: [ 
+                  { required: true, msg: 'Поле не может быть пустым' },
+                  { minLength: 3, msg: 'Введите не менее 3 сииволов' }
+                ], 
 
-    destinCity: [{
-      required: true,
-      msg: 'Поле не может быть пустым'
-    },{
-      minLength: 3,
-      msg: 'Введите не менее 3 сииволов'
-    }],
+    destinCity: [ 
+                  { required: true,msg: 'Поле не может быть пустым' },
+                  { minLength: 3,msg: 'Введите не менее 3 сииволов' }
+                ],
 
-    shippOptionsWeight: [{
-      required: true,
-      msg: 'Поле не может быть пустым'
-    },{
-      pattern: 'number',
-      msg: 'Введите число'
-    },
-    {
-      pattern: /^[0-9]\d*(\.\d+)?$/,
-      msg: 'Введите положительное число'
-    }],     
+    shippOptionsWeight: [ 
+                          { required: true, msg: 'Поле не может быть пустым' },
+                          { pattern: 'number', msg: 'Введите число' },
+                          { pattern: /^[0-9]\d*(\.\d+)?$/, msg: 'Введите положительное число' }
+                        ],     
 
-    shippOptionsVolume: [{
-      required: true,
-      msg: 'Поле не может быть пустым'
-    },{
-      pattern: 'number',
-      msg: 'Введите число'
-    },
-    {
-      pattern: /^[0-9]\d*(\.\d+)?$/,
-      msg: 'Введите положительное число'
-    }],
+    shippOptionsVolume: [ 
+                          { required: true, msg: 'Поле не может быть пустым' },
+                          { pattern: 'number', msg: 'Введите число' },
+                          { pattern: /^[0-9]\d*(\.\d+)?$/, msg: 'Введите положительное число' }
+                        ],
 
-    sizeLength: [{
-      required: true,
-      msg: 'Поле не может быть пустым'
-    },{
-      pattern: 'number',
-      msg: 'Введите число'
-    },
-    {
-      pattern: /^[0-9]\d*(\.\d+)?$/,
-      msg: 'Введите положительное число'
-    }], 
+    sizeLength: [ 
+                  { required: true, msg: 'Поле не может быть пустым' },
+                  { pattern: 'number', msg: 'Введите число' },
+                  { pattern: /^[0-9]\d*(\.\d+)?$/, msg: 'Введите положительное число' }
+                ], 
 
-    sizeWidth: [{
-      required: true,
-      msg: 'Поле не может быть пустым'
-    },{
-      pattern: 'number',
-      msg: 'Введите число'
-    },
-    {
-      pattern: /^[0-9]\d*(\.\d+)?$/,
-      msg: 'Введите положительное число'
-    }], 
+    sizeWidth: [
+                { required: true, msg: 'Поле не может быть пустым' },
+                { pattern: 'number', msg: 'Введите число' },
+                { pattern: /^[0-9]\d*(\.\d+)?$/, msg: 'Введите положительное число' }
+              ], 
 
-    sizeHeight: [{
-      required: true,
-      msg: 'Поле не может быть пустым'
-    },{
-      pattern: 'number',
-      msg: 'Введите число'
-    },
-    {
-      pattern: /^[0-9]\d*(\.\d+)?$/,
-      msg: 'Введите положительное число'
-    }]            
+    sizeHeight: [ 
+                  { required: true, msg: 'Поле не может быть пустым' },
+                  { pattern: 'number', msg: 'Введите число' },
+                  { pattern: /^[0-9]\d*(\.\d+)?$/, msg: 'Введите положительное число' }
+                ]            
   }       
 
 });
